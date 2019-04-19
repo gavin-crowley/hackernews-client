eb624d10491122b6e262f558bab7650b33b0689c
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2019-04-19 13:37:20 +0400

    app before breakup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,63 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
+
 const list = [
   {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
     num_comments: 3,
     points: 4,
-    objectID: 0
+    objectID: 0,
   },
   {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
     points: 5,
-    objectID: 1
-  }
+    objectID: 1,
+  },
 ];
+
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      list: list,
+      list,
+      searchTerm: '',
     };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
   }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        {this.state.list.map(item =>
+        <form>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {list.filter(isSearched(searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -38,6 +65,14 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         )}
       </div>
